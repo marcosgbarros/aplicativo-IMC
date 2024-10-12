b1b810cd53fc330e7323b7f0f89699af1f2d53fa
marcosgbarros <makymplay@gmail.com>
2024-10-12 02:12:05 -0300

    teste

--- server.js
+++ server.js
@@ -27,6 +27,28 @@ app.use(express.static(path.join(__dirname)));
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 
+//libera o CORS pro navegador
+module.exports = {
+  trailingSlash: false,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [];
+  }
+};
+
 
 // Rota POST para a API do ChatGPT
 app.post('/api/chatgpt', async (req, res) => {
