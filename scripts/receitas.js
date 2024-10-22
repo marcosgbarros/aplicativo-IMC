5dc7e86fead82eb905c8fb23fdc6a01353b58df4
Agnes Quenupe Martins <agnesknuppm@gmail.com>
2024-10-21 21:04:28 -0300

    wip

--- scripts/receitas.js
+++ scripts/receitas.js
@@ -87,25 +87,33 @@ const recipes = [
   }
 
   function showRecipeDetails(recipe) {
+    document.querySelector('.search-filter-container').style.display = 'none';
+    
     const grid = document.getElementById('recipeGrid');
     grid.innerHTML = '';
 
     const detailsDiv = document.createElement('div');
     detailsDiv.className = 'recipe-details';
     detailsDiv.innerHTML = `
-    <h2>${recipe.title}</h2>
-    <img src="${recipe.image}" alt="${recipe.title}" class="recipe-image-large">
-     <p><strong>Calorias:</strong> ${recipe.calories} calorias</p>
-    <p><strong>Ingredientes:</strong></p>
-    <ul>
-      ${recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
-    </ul>
+    <h1 class="recipe-title">${recipe.title}</h1>
+    <div class="recipe-content">
+      <img src="https://img.freepik.com/fotos-gratis/composicao-de-comida-brasileira-deliciosa-de-alto-angulo_23-2148739223.jpg" alt="${recipe.title}" class="recipe-image-large">
+      <div class="recipe-info-container">
+        <p><strong>Calorias:</strong> ${recipe.calories} calorias</p>
+        <p><strong>Ingredientes:</strong></p>
+        <ul>
+          ${recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
     <button id="backButton">Voltar</button>
     `;
 
     grid.appendChild(detailsDiv);
 
     document.getElementById('backButton').addEventListener('click', () => {
+      document.querySelector('.search-filter-container').style.display = 'block';
+    renderRecipes(recipes);
       renderRecipes(recipes);
     })
   }
